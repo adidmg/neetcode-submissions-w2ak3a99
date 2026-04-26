20bd31e4990259e744da0400f8e2e56c68dd0edd
adidmg <106352852+adidmg@users.noreply.github.com>
2026-04-26 18:48:29 +0530

    Add: non-cyclical-number - submission-5

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen=set()
+        while n!=1:
+            num_str=str(n)
+            total=0
+            for i in num_str:
+                total+=pow(int(i),2)
+                print(total)
+            if total==1:
+                return True
+            if total in seen:
+                return False
+            seen.add(total)
+            n=total
+        return True
